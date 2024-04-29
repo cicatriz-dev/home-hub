c27f6a9c920d8c0a7cf1c4a9aa6b666ea06b2ccf
Pedro Mello <contato@cicatriz.dev>
2024-04-29 20:31:23 -0300

    codigo aula 3.1

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -3,7 +3,13 @@ import { LifeCycles, registerApplication, start } from 'single-spa';
 registerApplication({
 	name: '@home-hub/hello-world',
 	app: () => System.import<LifeCycles>('@home-hub/hello-world'),
-	activeWhen: ['/'],
+	activeWhen: (location) => location.pathname === '/hello-world',
+});
+
+registerApplication({
+	name: '@home-hub/react-navbar',
+	app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
+	activeWhen: (location) => location.pathname === '/',
 });
 
 start({
