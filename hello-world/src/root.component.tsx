1f2457e824542ab5fc87ced73547e7a8dd0bf897
Pedro Mello <contato@cicatriz.dev>
2024-04-29 20:43:45 -0300

    codigo aula 3.2

--- hello-world/src/root.component.tsx
+++
@@ -1,3 +0,0 @@
-export default function Root(props) {
-	return <h1>Hello World!!</h1>;
-}
--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -1,12 +1,6 @@
 import { LifeCycles, registerApplication, start } from 'single-spa';
 
 registerApplication({
-	name: '@home-hub/hello-world',
-	app: () => System.import<LifeCycles>('@home-hub/hello-world'),
-	activeWhen: (location) => location.pathname === '/hello-world',
-});
-
-registerApplication({
 	name: '@home-hub/react-navbar',
 	app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
 	activeWhen: (location) => location.pathname === '/',
