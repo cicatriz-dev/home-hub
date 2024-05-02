264410fd5e59d33bbd53a03b7b96b2380e073298
Pedro Mello <contato@cicatriz.dev>
2024-05-02 20:50:56 -0300

    codigo aula 5.3

--- react-dashboard/src/components/UsersCard/index.tsx
+++ react-dashboard/src/components/UsersCard/index.tsx
@@ -1,12 +1,32 @@
+import { Box, Divider, Typography } from '@mui/material';
+
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Typography } from '@mui/material';
+import UsersIcons from '../../assets/users.png';
 
 const UsersCard = () => {
 	return (
 		<Card sx={{ background: '#F5F5F5' }}>
 			<CardContent>
-				<Typography>Users Card</Typography>
+				<Box display={'flex'} alignItems={'center'} flexDirection={'column'}>
+					<img src={UsersIcons} alt='water icon' width={60} />
+					<Typography color={'#E65100'}>Usuários da família</Typography>
+				</Box>
+				<Box display={'flex'} alignItems={'center'} justifyContent={'start'} sx={{ marginTop: 2 }}>
+					<AccountCircleIcon sx={{ color: '#BDBDBD', width: 60 }} />
+					<Typography variant='body2'>Usuário 1</Typography>
+				</Box>
+				<Divider sx={{ color: '#000000' }} style={{ marginTop: 8 }} />
+				<Box display={'flex'} alignItems={'center'} justifyContent={'start'} sx={{ marginTop: 1 }}>
+					<AccountCircleIcon sx={{ color: '#BDBDBD', width: 60 }} />
+					<Typography variant='body2'>Usuário 2</Typography>
+				</Box>
+				<Divider sx={{ color: '#000000' }} style={{ marginTop: 8 }} />
+				<Box display={'flex'} alignItems={'center'} justifyContent={'start'} sx={{ marginTop: 1 }}>
+					<AccountCircleIcon sx={{ color: '#BDBDBD', width: 60 }} />
+					<Typography variant='body2'>Usuário 3</Typography>
+				</Box>
 			</CardContent>
 		</Card>
 	);
--- react-dashboard/src/components/WaterCard/index.tsx
+++ react-dashboard/src/components/WaterCard/index.tsx
@@ -1,12 +1,32 @@
+import { Box, Typography } from '@mui/material';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { Typography } from '@mui/material';
+import { PieChart } from '@mui/x-charts';
+import WaterIcon from '../../assets/water.png';
 
 const WaterCard = () => {
 	return (
 		<Card sx={{ background: '#F5F5F5' }}>
 			<CardContent>
-				<Typography>Water Card</Typography>
+				<Box display={'flex'} flexDirection={'column'} alignItems={'center'}>
+					<img src={WaterIcon} alt='Water Icon' style={{ width: 50, height: 50 }} />
+					<Typography color={'#E65100'}>Water Card</Typography>
+					<PieChart
+						series={[
+							{
+								data: [
+									{ id: 0, value: 10, label: 'Chuveiros' },
+									{ id: 1, value: 15, label: 'Piscina' },
+									{ id: 2, value: 20, label: 'Lava e seca' },
+								],
+							},
+						]}
+						sx={{ marginTop: 2 }}
+						width={300}
+						height={131}
+					/>
+				</Box>
 			</CardContent>
 		</Card>
 	);
