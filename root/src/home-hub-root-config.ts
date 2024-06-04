4630738f36cb11323b673716969f7e67635003b5
Pedro Mello <contato@cicatriz.dev>
2024-06-04 18:09:28 -0300

    codigo aula 3.3 do curso 2

--- root/src/home-hub-root-config.ts
+++ root/src/home-hub-root-config.ts
@@ -3,19 +3,19 @@ import { LifeCycles, registerApplication, start } from 'single-spa';
 registerApplication({
 	name: '@home-hub/react-navbar',
 	app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
-	activeWhen: (location) => location.pathname.includes('/'),
+	activeWhen: ['/dashboard/:id/', '/dashboard/#/'],
 });
 
 registerApplication({
 	name: '@home-hub/react-dashboard',
 	app: () => System.import<LifeCycles>('@home-hub/react-dashboard'),
-	activeWhen: (location) => location.pathname.includes('/'),
+	activeWhen: ['/dashboard/:id/', '/dashboard/#/'],
 });
 
 registerApplication({
 	name: '@home-hub/react-login',
 	app: () => System.import<LifeCycles>('@home-hub/react-login'),
-	activeWhen: ['/login/#/', (location) => location.pathname === '/'],
+	activeWhen: (location) => location.pathname === '/',
 });
 
 start({
