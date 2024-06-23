9d3258209713ff50f486ef6f06aa47f907a494a2
Pedro Mello <contato@cicatriz.dev>
2024-06-23 17:01:59 -0300

    codigo aula 1.3 do curso 3

--- utils/src/home-hub-utils.ts
+++ utils/src/home-hub-utils.ts
@@ -1,4 +1,12 @@
+import { FormValues } from './types';
+
 // Anything exported from this file is importable by other in-browser modules.
 export function publicApiFunction() {
 	console.log('Hello World! Módulo utililitário');
 }
+
+export const loginFunction = (data: FormValues) => {
+	const authId = data.email.replace('@', '').replace('.', '').codePointAt(0);
+	localStorage.setItem('auth', JSON.stringify(data));
+	return location.replace(`/dashboard/${authId}/`);
+};
