46e6bc946a5d2addb664961d23516949df4430a8
Pedro Mello <contato@cicatriz.dev>
2024-09-25 19:14:56 -0300

    codigo curso 6 aula 4.1

--- packages/root/src/home-hub-root-config.ts
+++ packages/root/src/home-hub-root-config.ts
@@ -1,23 +1,23 @@
-import { LifeCycles, registerApplication, start } from 'single-spa';
+import { LifeCycles, registerApplication, start } from "single-spa";
 
 registerApplication({
-	name: '@home-hub/react-navbar',
-	app: () => System.import<LifeCycles>('@home-hub/react-navbar'),
-	activeWhen: ['/dashboard/:id/', '/dashboard/#/'],
+  name: "@home-hub/react-navbar",
+  app: () => System.import<LifeCycles>("@home-hub/react-navbar"),
+  activeWhen: ["/dashboard/:id/", "/dashboard/#/"],
 });
 
 registerApplication({
-	name: '@home-hub/react-dashboard',
-	app: () => System.import<LifeCycles>('@home-hub/react-dashboard'),
-	activeWhen: ['/dashboard/:id/', '/dashboard/#/'],
+  name: "@home-hub/react-dashboard",
+  app: () => System.import<LifeCycles>("@home-hub/react-dashboard"),
+  activeWhen: ["/dashboard/:id/", "/dashboard/#/"],
 });
 
 registerApplication({
-	name: '@home-hub/react-login',
-	app: () => System.import<LifeCycles>('@home-hub/react-login'),
-	activeWhen: (location) => location.pathname === '/',
+  name: "@home-hub/react-login",
+  app: () => System.import<LifeCycles>("@home-hub/react-login"),
+  activeWhen: (location) => location.pathname === "/",
 });
 
 start({
-	urlRerouteOnly: true,
+  urlRerouteOnly: true,
 });
